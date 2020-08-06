35ab44b006e93fbb4f72ebf35177738204be9773
michael thomas <michael.thomas2014@gmail.com>
2020-08-06 17:01:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Write your code here!
+let newHeader =document.createElement('h1#victory')
+
+// document.body.appendChild(element)
+
 let element = document.body
 
 element.querySelector("main#main").remove()
 
-newHeader = element.createElement('newHeader')
+// newHeader = element.createElement('newHeader')
 
 

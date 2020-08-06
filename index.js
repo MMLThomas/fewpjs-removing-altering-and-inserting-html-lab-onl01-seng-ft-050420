b22ed185721a859876f58d1d403e8616425c12af
michael thomas <michael.thomas2014@gmail.com>
2020-08-06 17:05:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ let element = document.body
 
 element.querySelector("main#main").remove()
 
-let newHeader = document.createElement('h1#victory')
+let header = document.createElement('h1#victory')
+
+let newHeader = document.querySelector("h1#victory")
+
